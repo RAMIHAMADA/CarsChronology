9af65e5d591522bbd9855a7fd23b72301dd55c96
Khamadekh Rami <khamadekh@gmail.com>
2023-10-04 16:41:00 +0300

    added new packages and models

---
+++ data/src/main/java/com/rami/data/retrofit/models/PostData.kt
@@ -0,0 +1,25 @@
+package com.rami.data.retrofit.models
+
+import com.google.gson.annotations.SerializedName
+
+data class PostData(
+    val id: Long,
+    val text: String,
+
+    @SerializedName("like_count")
+    val likeCount: Long,
+
+    @SerializedName("created_at")
+    val createdAt: String,
+
+    @SerializedName("comment_count")
+    val commentCount: Long,
+
+    val img: String? = "",
+    val author: User,
+)
+
+data class PostDataResponse(
+    val posts: List<PostData>,
+    val user: User,
+)
