f18f18e202ec1fdc5bd6035e62bcf4f1f1c4b5d7
Khamadekh Rami <khamadekh@gmail.com>
2023-10-06 15:14:28 +0300

    added new layout item_post.xml and navigation in SplashFragment.kt

--- app/src/main/java/com/rami/taxi/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/rami/taxi/ui/splash/SplashFragment.kt
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.rami.taxi.R
 import com.rami.taxi.databinding.FragmentSplashBinding
@@ -13,12 +14,18 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
     private val binding: FragmentSplashBinding by viewBinding()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        startAnimations()
     }
 
     private fun startAnimations() {
         binding.lottieCar.addAnimatorListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animator: Animator) {
+                navigateNext()
             }
         })
+    }
+
+    private fun navigateNext() {
+        findNavController().navigate(R.id.action_splashFragment_to_carListFragment)
     }
 }
